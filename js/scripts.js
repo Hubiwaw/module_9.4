d1d6a67d7c2329a01f389cc3e7d25caeebbcc47b
Hubert Maćkowiak <mackowiak.h@wp.pl>
2017-11-07 20:28:43 +0100

    corrections in js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function drawTree(a) {
-	for (var i = 0; i < a; i++) {
-		var star="";
+function drawTree(level) {
+	for (var i = 0; i < level; i++) {
+		var star= "";
 		for (var j = 0; j <= i; j++) {
-		star += "*";
+			star += "*";
 		}
 		console.log(star);
 	}
